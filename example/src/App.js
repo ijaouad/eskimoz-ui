be243df0a55c6035b97272318904e04497102c0d
JAOUAD I <ichijawad@gmail.com>
2023-10-13 09:37:25 +0100

    add example

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from '../../src/index'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button size="large">Hello</Button>
+    </div>
+  );
+}
+
+export default App;
